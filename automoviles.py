26626bce060ae80efd38271466c41706b4811d65
jfdaza <mainekko23@gmail.com>
2022-09-01 17:03:42 -0300

    modificacion de ruta, argumentos del metodo

--- automoviles.py
+++ automoviles.py
@@ -7,7 +7,7 @@ Terrestres = terrestres.Terrestres
 
 class Automoviles(Terrestres):
 
-    def __init__(self, matricula, tipo_automovil, color, traccion, cilindraje, torque):
+    def __init__(self, matricula, tipo_automovil, color=None, traccion=None, cilindraje=None, torque=None):
         # Vehiculo.__init__(matricula, marca, modelo)
         # Terrestres.__init__(nro_ruedas)
         self.matricula = matricula
@@ -17,17 +17,17 @@ class Automoviles(Terrestres):
         self.cilindraje = cilindraje
         self. torque = torque
 
-    def get_automovil(self, matricula):
-        with open("./Transito/data/automoviles.txt", "r") as automoviles:
+    def get_automovil(self, matricula, tipo):
+        with open("C:/Users/jfdaza/Documents/Python/Transito/data/automoviles.txt", "r") as automoviles:
             for auto in automoviles:
                 data = auto.split('|')
                 if  matricula == data[0]:
                     automoviles.close()
-                    Myvehiculo = Vehiculo()
-                    Portierra = Terrestres()
+                    Myvehiculo = Vehiculo(matricula, tipo)
+                    Portierra = Terrestres(matricula, tipo)
                     data_vehiculo = Myvehiculo.get_vehiculo(data[0])
-                    data_vehiculo_terrestre = Portierra.get_terrestre(data[0])
-                    return data + data_vehiculo + data_vehiculo_terrestre
+                    data_vehiculo_terrestre = Portierra.get_terrestre(data[0], data[1])
+                    return data_vehiculo_terrestre
             
             else:
                 automoviles.close()
